8f5fff26f60a4017a3df9c4f40dc6ae4652f9e65
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2010-10-13 15:17:01 +0900

    Fixed copy command on build.sh.

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ COPY_TARGET_PATH=../gunyarapaint/libs
 
 # ** BUILD **
 $FLEX3_SDK_PATH/bin/compc -benchmark=true -incremental=true -optimize=true -target-player=10.0.0 -verbose-stacktraces=true -include-sources src -source-path ./src -output ./bin/framework.swc
-copy ./bin/framework.swc $COPY_TARGET_PATH
+cp ./bin/framework.swc $COPY_TARGET_PATH
